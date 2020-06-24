1eff67eac3cc346d9f1896da65fa5dbbf5955c31
noelrmrz <noel.rmrz@gmail.com>
2020-06-24 11:51:26 +0400

    finished troubleshooting AsyncEndPointTest

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointAsyncTaskTest.java
@@ -1,20 +1,32 @@
 package com.udacity.gradle.builditbigger;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class EndpointAsyncTaskTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
-    public void testDoInBackground() throws Exception {
-        com.udacity.gradle.builditbigger.MainActivity mainActivity = new com.udacity.gradle.builditbigger.MainActivity();
-        mainActivity.testFlag = true;
-        new EndpointAsyncTask().execute(mainActivity);
-        Thread.sleep(5000);
-        assertNotNull("Error:  Fetched joke = " + mainActivity.getJoke(), mainActivity.getJoke());
+    public void testAsyncTask() throws InterruptedException, ExecutionException {
+        // on the MainActivity execute the AsyncTask
+        EndpointAsyncTask asyncTaskTask = new EndpointAsyncTask();
+        asyncTaskTask.execute(activityTestRule.getActivity());
+
+        // the String returned in the onPostExecute is being retrieved
+        String randomJoke = asyncTaskTask.get();
+
+        // If the string is not null, then we got a value
+        assertNotNull(randomJoke);
     }
 }
