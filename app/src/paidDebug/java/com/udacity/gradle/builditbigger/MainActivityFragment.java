a4461668f5420e71e01faa7cdd8b59034314b583
noelrmrz <noel.rmrz@gmail.com>
2020-06-24 10:43:44 +0400

    completed free and paid product flavors

---
+++ app/src/paidDebug/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,29 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public boolean testFlag;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Remove code for Ads for paid variant
+
+        return root;
+    }
+}
